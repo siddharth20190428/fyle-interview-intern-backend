c4a35594625b18be0c2f3e4ab72801f103a19d00
Siddharth Sahu <siddharth201820@gmail.com>
2024-10-03 02:14:57 +0530

    POST /principal/assignments/grade done

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -5,7 +5,7 @@ from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
 from core.models.teachers import Teacher
 
-from .schema import AssignmentSchema, TeacherSchema
+from .schema import AssignmentSchema, AssignmentGradeSchema, TeacherSchema
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
 
 @principal_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
@@ -23,3 +23,19 @@ def list_teachers(p):
     teachers = Teacher.get_all_teachers()
     teachers_dump = TeacherSchema().dump(teachers, many=True)
     return APIResponse.respond(data=teachers_dump)
+
+@principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignment(p, incoming_payload):
+    """Grade an assignment"""
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.mark_grade(
+        _id=grade_assignment_payload.id,
+        grade=grade_assignment_payload.grade,
+        auth_principal=p
+    )
+    db.session.commit()
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
